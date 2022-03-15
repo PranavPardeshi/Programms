9df68e3ac85c4e42bf3c0802d75727ce6e1e3229
PranavPardeshi <89290133+PranavPardeshi@users.noreply.github.com>
2022-03-15 23:06:57 +0530

    Add files via upload

---
+++ C/Program69.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main()
+{
+    int Arr[5];
+	int iSum = 0;
+	
+	printf("Enter numbers: ");
+	
+	scanf("%d",&Arr[0]);
+	scanf("%d",&Arr[1]);
+	scanf("%d",&Arr[2]);
+	scanf("%d",&Arr[3]);
+	scanf("%d",&Arr[4]);
+	
+	iSum = Arr[0] + Arr[1] + Arr[2] + Arr[3] + Arr[4];
+	
+	printf("Addition is: %d",iSum);
+	
+    return 0;
+}
---
+++ C/program74.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+
+
+int main()
+{
+	int arr[5];
+	int ret = 0,i = 0,a = 0;
+	
+	cout<<"Enter numbers"<<endl;
+	
+	for(i = 0; i < 5;i++)
+	{
+		cin>>arr[i];
+	}
+	
+	cout<<arr<<endl;
+	return 0;
+}
